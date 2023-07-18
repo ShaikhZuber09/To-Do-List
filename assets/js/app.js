6db0bad58f5ed848f4e650029c2bc28ded8243ed
ShaikhZuber09~ <zs618272@gmail.com>
2023-07-18 20:04:50 +0530

    work on localstorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,9 @@ const toDoInput=document.getElementById("toDoInput");
 
 const templating=(arr)=>{
     let result=``;
-    arr.forEach(ele=>{
+    arr.forEach(obj=>{
         result +=` <li class="list-group-item toDoItem font-weight-bold text-uppercase d-flex justify-content-between">
-        <span>${ele}</span>
+        <span>${obj.skillName}</span>
         <span>
           <i class="fa-solid fa-pen-to-square edit"></i>
           <i class="fa-solid fa-trash-can delete ml-2"></i>
@@ -17,11 +17,22 @@ const templating=(arr)=>{
     });
     toDoList.innerHTML=result;
 }
-let skillArr=[];
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+let skillArr=JSON.parse(localStorage.getItem("skillArr"))||[];
+templating(skillArr)
 const onSubmitAdd=(eve)=>{
     eve.preventDefault();
     let skill=toDoInput.value
-    skillArr.push(skill)
+    let obj={
+        skillName:skill,
+        uuid:uuidv4()
+    }
+    skillArr.push(obj)
+    localStorage.setItem("skillArr", JSON.stringify(skillArr))
    templating(skillArr)
     toDoForm.reset()
 }
