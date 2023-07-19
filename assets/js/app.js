67cf6a7b31b94cb2569be1b6c76742c580fac18a
ShaikhZuber09~ <zs618272@gmail.com>
2023-07-20 02:25:44 +0530

    work on edit & delete functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,49 @@ const cl=console.log;
 const toDoForm=document.getElementById("toDoForm");
 const toDoList=document.getElementById("toDoList");
 const toDoInput=document.getElementById("toDoInput");
+const addBtn=document.getElementById("addBtn");
+const updateBtn=document.getElementById("updateBtn");
 
+const onEdit=(eve)=>{
+let editId=eve.getAttribute("data-id")
+let editObj=skillArr.find((obj)=>{
+  return ( obj.uuid===editId)
+})
+localStorage.setItem("editId", editId)
+toDoInput.value=editObj.skillName;
+addBtn.classList.add("d-none")
+updateBtn.classList.remove("d-none")
+}
+const onUpdate=(eve)=>{
+  let updatedValue=toDoInput.value
+  let updateId=localStorage.getItem("editId")
+  skillArr.forEach(obj=>{
+    if(obj.uuid === updateId){
+      obj.skillName=updatedValue
+    }
+  })
+  localStorage.setItem("skillArr", JSON.stringify(skillArr))
+  templating(skillArr)
+  toDoForm.reset()
+  addBtn.classList.remove("d-none")
+updateBtn.classList.add("d-none")
+}
+updateBtn.addEventListener("click", onUpdate)
+
+const onDelete=(eve)=>{
+  let deleteId=eve.getAttribute("data-id")
+  skillArr=skillArr.filter(obj=> obj.uuid != deleteId)
+  localStorage.setItem("skillArr", JSON.stringify(skillArr))
+  templating(skillArr)
+}
 const templating=(arr)=>{
     let result=``;
     arr.forEach(obj=>{
         result +=` <li class="list-group-item toDoItem font-weight-bold text-uppercase d-flex justify-content-between">
         <span>${obj.skillName}</span>
         <span>
-          <i class="fa-solid fa-pen-to-square edit"></i>
-          <i class="fa-solid fa-trash-can delete ml-2"></i>
+          <i class="fa-solid fa-pen-to-square edit" onclick="onEdit(this)" data-id="${obj.uuid}"></i>
+          <i class="fa-solid fa-trash-can delete ml-2 "onclick="onDelete(this)" data-id="${obj.uuid}"></i>
         </span>
       </li>`
     });
